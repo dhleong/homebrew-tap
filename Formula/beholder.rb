9e9acd5d97f5318b61055d2774609a5f10937256
Daniel Leong <falcone88@gmail.com>
2025-09-21 22:21:01 -0400

    Update for v1.8.1

--- Formula/beholder.rb
+++ Formula/beholder.rb
@@ -2,7 +2,7 @@ class Beholder < Formula
   desc "CLI tool for D&D players"
   homepage "https://github.com/dhleong/beholder"
   url "https://github.com/dhleong/beholder/archive/1.8.1.tar.gz"
-  sha256 "-172dc0816d733ec2a2910af62e0846e993fbc40c01a28ae4a510af699881ba90"
+  sha256 "e8d23f7e928cc13d5d6ef509d1f7b9166c043bf3fe5d751b5aef5096677e4570"
 
   depends_on "go" => :build
 
